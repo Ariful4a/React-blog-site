d55497a43cc705f6c555d400eeddd5de322d3a3d
Ariful4a <afkgamingno1@gmail.com>
2025-01-18 09:20:54 -0400

    add my all code and file

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">Konwlaedge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
