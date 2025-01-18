3ee8d0fafe61b74fb36e6b9ddb1b321b5af5c2fe
Ariful4a <afkgamingno1@gmail.com>
2025-01-18 10:07:36 -0400

    add blog.jsx section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@
 import './App.css'
 import Header from './Components/Header/Header'
 import Blogs from './Components/Blogs/Blogs'
+import Bookmark from './Components/Bookmark/Bookmark'
 
 function App() {
 
   return (
     <>
       <Header></Header>
+      <div className="md:flex">
       <Blogs></Blogs>
+      <Bookmark></Bookmark>
+      </div>
     </>
   )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline text-red-500">Konwlaedge Cafe</h1>
-        </div>
+        <header className="flex justify-between items-center p-2 mx-4 border-b-2">
+            <h1 className="text-3xl font-black text-red-500">Konwlaedge Cafe</h1>
+            <img className="w-12 h-12 rounded-full" src={profile} alt="" />
+        </header>
     );
 };
 
