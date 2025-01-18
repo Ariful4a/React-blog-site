36a7e0c4dadec6654fdbc0d315d2c5c223684bd5
Ariful4a <afkgamingno1@gmail.com>
2025-01-18 11:30:30 -0400

    add all blog cover and title and code

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {cover, title, img, author, posted_date, reading_time, hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className="w-full mb-8" src={cover} alt={`Cover of ${title}`} />
+            <div className="flex justify-between items-center">
+                <div className="flex gap-3 items-center">
+                    <img className="w-14 h-14 rounded-full" src={img} alt="" />
+                    <div>
+                        <h3 className="font-bold text-2xl">{author}</h3>
+                        <p className="text-base text-[#11111199] font-semibold">{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className="text-base text-[#11111199] font-semibold">{reading_time} min read</p>
+                </div>
+            </div>
+            <h2 className="text-4xl font-bold mt-5 mb-3">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hashtag, index) => <span key={index} className="text-base text-[#11111199] font-semibold"> {hashtag}</span>)
+                }
+            </p>
         </div>
     );
 };
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import profile from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <header className="flex justify-between items-center p-2 mx-4 border-b-2">
+        <header className="flex justify-between items-center p-2 mb-6 border-b-2 max-w-6xl mx-auto">
             <h1 className="text-3xl font-black text-red-500">Konwlaedge Cafe</h1>
             <img className="w-12 h-12 rounded-full" src={profile} alt="" />
         </header>
